0a5333ffd0eb586c5fc9d1d09d7b4ff130344373
shubh0786 <shubham.singh267@gmail.com>
2021-01-28 17:55:36 +1300

    First Array

--- ArrayProblems/Program.cs
+++ ArrayProblems/Program.cs
@@ -11,9 +11,25 @@ namespace ArrayProblems
         static void Main(string[] args)
         {
 
+            int[] arr = new int[10];
+            int i;
 
+            Console.WriteLine("please input 10 numbers");
+            for (i = 0; i < 10; i++)
+            {
+                Console.WriteLine("following are the inputs for {0} its : ",i);
+                arr[i] = int.Parse(Console.ReadLine());
+            }
 
+            Console.WriteLine("Elements in array are : ");
+            for ( i = 0; i < 10; i++)
+            {
 
+                Console.WriteLine("{0} " , arr[i]);
+
+            }
+
+            Console.ReadLine();
 
         }
     }
